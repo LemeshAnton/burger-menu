b2decf3264ecb3fbe34c643ed7db1f4312afd76a
LemeshAnton <76262088+LemeshAnton@users.noreply.github.com>
2022-12-12 18:42:24 +0300

    add burger

---
+++ script.js
@@ -0,0 +1,6 @@
+let burgerBtn = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__menu');
+burgerBtn.addEventListener('click', function(){
+    burgerBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+});
