7bbff7af4ba2da3d0c2bde4332400ab239b9d00a
LemeshAnton <76262088+LemeshAnton@users.noreply.github.com>
2022-12-21 14:16:15 +0300

    create section with change title

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const burgerBtn = document.querySelector('.header__button');
 const menu = document.querySelector('.header__menu');
-burgerBtn.addEventListener('click', function(){
+const closeTitle = document.querySelector('.section__title--close');
+const openTitle = document.querySelector('.section__title--open');
+
+
+  burgerBtn.addEventListener('click', function(){
     burgerBtn.classList.toggle('active');
     menu.classList.toggle('active');
+    closeTitle.classList.toggle('close');
+    openTitle.classList.toggle('open');
 });
